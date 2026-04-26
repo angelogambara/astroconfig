5a5ea38d8b55dac716abda0f2fafdfff2dede610
Angelo Gambara <angelo@angelogambara.dev>
2026-04-26 08:11:34 +0200

    refactor: polish.lua -> astrocore.lua

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -10,76 +8,48 @@ return {
   "AstroNvim/astrocore",
   ---@type AstroCoreOpts
   opts = {
-    -- Configure core features of AstroNvim
-    features = {
-      large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
-      cmp = true, -- enable completion at start
-      diagnostics = { virtual_text = true, virtual_lines = false }, -- diagnostic settings on startup
-      highlighturl = true, -- highlight URLs at start
-      notifications = true, -- enable notifications at start
-    },
-    -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-    diagnostics = {
-      virtual_text = true,
-      underline = true,
-    },
-    -- passed to `vim.filetype.add`
     filetypes = {
-      -- see `:h vim.filetype.add` for usage
       extension = {
-        foo = "fooscript",
-      },
-      filename = {
-        [".foorc"] = "fooscript",
+        ejs = "embedded_template",
+        gohtml = "gotmpl",
+        gotmpl = "gotmpl",
+        j2 = "jinja",
+        jinja2 = "jinja",
+        njk = "jinja",
       },
       pattern = {
-        [".*/etc/foo/.*"] = "fooscript",
+        [".*/views/.*%.html"] = "embedded_template",
       },
     },
-    -- vim options can be configured here
     options = {
-      opt = { -- vim.opt.<key>
-        relativenumber = true, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
-        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = false, -- sets vim.opt.wrap
-      },
-      g = { -- vim.g.<key>
-        -- configure global vim variables (vim.g)
-        -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
-        -- This can be found in the `lua/lazy_setup.lua` file
+      opt = {
+        clipboard = vim.env.SSH_CONNECTION and "" or "unnamedplus",
       },
     },
-    -- Mappings can be configured through AstroCore as well.
-    -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
     mappings = {
-      -- first key is the mode
       n = {
-        -- second key is the lefthand side of the map
-
-        -- navigate buffer tabs
-        ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-        ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
-
-        -- mappings seen under group name "Buffer"
-        ["<Leader>bd"] = {
-          function()
-            require("astroui.status.heirline").buffer_picker(
-              function(bufnr) require("astrocore.buffer").close(bufnr) end
-            )
-          end,
-          desc = "Close buffer from tabline",
-        },
-
-        -- tables with just a `desc` key will be registered with which-key if it's installed
-        -- this is useful for naming menus
-        -- ["<Leader>b"] = { desc = "Buffers" },
-
-        -- setting a mapping to false will disable it
-        -- ["<C-S>"] = false,
+        ["<Leader>w"] = { desc = "Vimwiki" },
       },
     },
   },
+  ---@param opts AstroCoreOpts
+  config = function(_, opts)
+    local runtime_dir = vim.env.XDG_RUNTIME_DIR .. "/nvim"
+
+    for _, dir in ipairs { "shada", "undo" } do
+      vim.fn.mkdir(runtime_dir .. "/" .. dir, "p", "0700")
+    end
+
+    -- Extend AstroNvim configuration
+    opts = vim.tbl_deep_extend("force", opts, {
+      options = {
+        opt = {
+          shadafile = runtime_dir .. "/shada/main.shada",
+          undodir = runtime_dir .. "/undo",
+        },
+      },
+    })
+
+    require("astrocore").setup(opts)
+  end,
 }
--- lua/polish.lua
+++ lua/polish.lua
@@ -1,25 +1,5 @@
+if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
-
-vim.o.clipboard = vim.env.SSH_CONNECTION and "" or "unnamedplus"
-
-local runtime_dir = vim.env.XDG_RUNTIME_DIR .. "/nvim"
-local shada_dir = runtime_dir .. "/shada"
-local shada_file = shada_dir .. "/main.shada"
-local undo_dir = runtime_dir .. "/undo"
-
-vim.fn.mkdir(shada_dir, "p", "0700")
-vim.fn.mkdir(undo_dir, "p", "0700")
-
-vim.o.shadafile = shada_file
-vim.o.undodir = undo_dir
-
-vim.filetype.add {
-  pattern = {
-    [".*/views/.*%.html"] = "embedded_template",
-  },
-  extension = {
-    ejs = "embedded_template",
-  },
-}
