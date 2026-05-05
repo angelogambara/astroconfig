9fd7a3f9d71361654f6b446159e4336dcbba8212
Angelo Gambara <angelo@angelogambara.dev>
2026-05-05 23:49:49 +0200

    fix(java): add formatting style

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -23,6 +23,13 @@ return {
               },
             },
           },
+          format = {
+            enabled = true,
+            settings = {
+              url = "https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml",
+              profile = "GoogleStyle",
+            },
+          },
         },
       },
     },
